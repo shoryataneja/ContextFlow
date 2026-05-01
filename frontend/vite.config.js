98a299542c41be6637b26886008a670392622e77
Shorya Taneja <shoryataneja5@gmail.com>
2026-05-01 15:31:05 +0530

    debug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts'],
+          http: ['axios'],
+        },
+      },
+    },
+  },
   server: {
     port: 5173,
     proxy: {
